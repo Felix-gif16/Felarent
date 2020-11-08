5bd51db868c80ad3dbe88b5b5df13aac6933741b
Felix-gif16 <felixcobbinah4@gmail.com>
2020-11-07 17:49:55 -0800

    first commit

---
+++ database/migrations/2020_11_05_101806_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        schema::create('message', function(Blueprint $table){
+
+            $table->increament('id');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        schema::drop('');
+    }
+}
